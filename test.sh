94f35948437f451fa8ac387a81a48fb5609400c7
Erik Dykema <erik@caserails.com>
2014-01-12 14:26:13 -0500

    Update CI Scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "Testing"
-pip install --use-wheel --find-links=dist/ django-ng-pdfviewer
+pip install --use-wheel --find-links=dist/ $PACKAGE_NAME
 echo "import ng_pdfviewer; print 'Hello World';" | python
