92792856ebfcf3aca463fd56a2b43acbd86bafe7
Erik Dykema <erik@caserails.com>
2014-01-17 23:54:10 -0500

    testing

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 set -e
 export PIP_DOWNLOAD_CACHE=~/.pip_cache/
 export PIP_FIND_LINKS="$PIP_DOWNLOAD_CACHE"
+rm -rf /home/rof/.virtualenv/lib/python2.7/site-packages/django_ng_*
 pip install --upgrade $PACKAGE_NAME
 echo "import $PACKAGE_NAME; print 'Hello World';" | python
